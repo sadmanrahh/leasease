9fd9f3829642c6141696815a6f22d751bbaf43d7
Mosfeq <mosfeq5000@gmail.com>
2022-12-24 23:00:27 +0000

    firebase connected with authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire/compat'
+import { environment } from 'src/environment/environment';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { OverviewComponent } from './component/overview/overview.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase : {
+        apiKey: "AIzaSyD9adN001ig2CAbf3vjAp7_R-yp0L4rKes",
+        authDomain: "leasewithease.firebaseapp.com",
+        databaseURL: "https://leasewithease-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "leasewithease",
+        storageBucket: "leasewithease.appspot.com",
+        messagingSenderId: "691041306997",
+        appId: "1:691041306997:web:64c4fdd26f7837c2b02b3c"
+      }
+}
